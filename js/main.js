f01c0868e89a566027915ba39726068020003466
橋本大徳 <hashimotohironori@hashimotohironorinoMacBook-Pro.local>
2020-02-02 18:36:46 +0900

    first-comit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+var app = new Vue({
+    el: '#app',
+    data:{
+        items:null,
+        keyword:'',
+        message:''
+    },
+    watch:{
+        keyword:function(newKeyword,oldKeyword){
+            this.message="waiting for you stop typing..."
+            this.debouncedGetAnswer()
+        }
+    },
+    created: function(){
+
+        this.debouncedGetAnswer=_.debounce(this.getAnswer,1000)
+    },
+    methods:{
+        getAnswer:function(){
+            if(this.keyword===''){
+                this.items = null
+                this.message=""
+                return
+            }
+            this.message='Loading...'
+            var vm=this
+            var params={ page:1, per_page:20,query:this.keyword}
+            axios.get('https://qiita.com/api/v2/items',{params})
+            .then(function(response){
+                console.log(response)
+                vm.items= response.data
+            })
+            .catcj(function(error){
+                vm.message='error'+error
+            })
+            .finaly(function(){
+                vm.message=''
+            })
+        }
+    }
+})
